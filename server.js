f59715f489fc0d75a3e20e27902dfb9b99856247
mxwllndrsn <mxwllndrsn@gmail.com>
2019-08-24 01:33:56 -0700

    succesful template render

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // server.js
 
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.locals.basedir = path.join(__dirname, './')
 
 app.get('/', function (req, res) {
-	res.send('Hello, World.')
+	res.render('index')
 })
 
 app.listen(3000, function () {
