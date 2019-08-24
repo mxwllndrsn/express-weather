f5f8a623c04a54be3bf7bbdbdb5380640001b788
mxwllndrsn <mxwllndrsn@gmail.com>
2019-08-24 02:30:26 -0700

    static css file success

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
+//setup/////////////////////////////////
+
 app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 app.locals.basedir = path.join(__dirname, './')
+app.use(express.static(path.join(__dirname, 'public')));
+
+//function//////////////////////////////
 
 app.get('/', function (req, res) {
 	res.render('index')
+})
+
+app.post('/', function (req, res) {
+	//
 })
 
 app.listen(3000, function () {
